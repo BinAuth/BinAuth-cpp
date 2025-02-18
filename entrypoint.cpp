008f791216f3240f7293575b368bcd1823e8b4c4
BinAuth <binauthentication@gmail.com>
2025-02-18 21:48:10 +0000

    Create entrypoint.cpp

---
+++ entrypoint.cpp
@@ -0,0 +1,23 @@
+void BinAuthentication::setup( std::string license )
+{
+	  this->webhook = " ";
+
+    this->api_key = " ";
+
+	  this->log_activity( this->webhook );
+
+    this->initalize_auth( this->api_key );
+
+    this->validate_discord( license );
+
+    this->validate_key( license );
+}
+
+int main( ) 
+{
+
+  std::string license = " ";
+
+	BinAuth.get()->setup( license );
+
+}
